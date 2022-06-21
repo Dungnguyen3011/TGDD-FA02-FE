01e2d181db4606438d634cc69a7fef9a4bc097ff
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-06-21 17:55:45 +0700

    add project into repo

---
+++ src/main/java/com/project/tgdd_fe/TgddFeApplication.java
@@ -0,0 +1,13 @@
+package com.project.tgdd_fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgddFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgddFeApplication.class, args);
+	}
+
+}
